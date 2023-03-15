8070e9bcab7a3ab912527c2dde9623dcb3d4ed62
davisbento <contato@davisbento.dev>
2023-03-15 10:43:41 -0300

    tests: category test

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -1,5 +1,43 @@
+import { Category } from './category';
+
 describe('Category', () => {
 	it('should be defined', () => {
-		expect(true).toBe(true);
+		expect(new Category({ name: 'Category name' })).toBeDefined();
+	});
+
+	it('should be able to create a new category', () => {
+		const category = new Category({ name: 'Category name' });
+		expect(category.name).toBe('Category name');
+	});
+
+	it('should be able to create a new category with description', () => {
+		const category = new Category({
+			name: 'Category name',
+			description: 'Category description'
+		});
+
+		expect(category.name).toBe('Category name');
+		expect(category.description).toBe('Category description');
+	});
+
+	it('should be able to create a new category with is_active set to false', () => {
+		const category = new Category({
+			name: 'Category name',
+			is_active: false
+		});
+
+		expect(category.name).toBe('Category name');
+		expect(category.is_active).toBe(false);
+	});
+
+	it('should be able to create a new category without a created date and returning the default date', () => {
+		const now = new Date();
+		const category = new Category({
+			name: 'Category name'
+		});
+
+		expect(category.name).toBe('Category name');
+		expect(category.created_at).toBeInstanceOf(Date);
+		expect(category.created_at).toEqual(now);
 	});
 });
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,7 +1,42 @@
-class Category {
-	constructor(public name: string) {}
+export type CategoryProps = {
+	name: string;
+	description?: string;
+	is_active?: boolean;
+	created_at?: Date;
+};
 
-	public static create(id: string): Category {
-		return new Category(id);
+export class Category {
+	constructor(public readonly props: CategoryProps) {
+		this.props.is_active = this.props.is_active ?? true;
+		this.props.description = this.props.description || undefined;
+		this.props.created_at = this.props.created_at || new Date();
+	}
+
+	get name(): string {
+		return this.props.name;
+	}
+
+	get is_active(): boolean {
+		return this.props.is_active ?? true;
+	}
+
+	get description(): string {
+		return this.props.description || '';
+	}
+
+	get created_at(): Date {
+		return this.props.created_at || new Date();
+	}
+
+	private set name(name: string) {
+		this.props.name = name;
+	}
+
+	private set description(description: string) {
+		this.props.description = description;
+	}
+
+	private set is_active(is_active: boolean) {
+		this.props.is_active = is_active;
 	}
 }
